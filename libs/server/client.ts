adbcab08ad6d4beaf8ce4d5309180cb75a52ff2c
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-08-05 20:26:40 +0900

    db add 09

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from ".prisma/client";
 declare global {
 	var client: PrismaClient | undefined;
 }
-const client = global.client || new PrismaClient();
+const client = new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.client = client;
 
