00c42d5a597363cb4634b11ae57d63a3ea0dcd37
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-08-06 15:01:04 +0900

    testpage 02

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className="w-full max-w-screen-2xl  h-screen mx-auto">
+		<div className="w-full max-w-screen-2xl  h-screen ">
 			<Component {...pageProps} />
 		</div>
 	);
