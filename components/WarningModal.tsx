b6af29127fca1a3c707524caa2d8315e55c3e690
shine9687 <sinhyeonsu@MacBook-Pro.local>
2022-08-16 14:57:49 +0900

    result test  39

--- components/WarningModal.tsx
+++ components/WarningModal.tsx
@@ -14,6 +14,7 @@ const WarningModal = ({ handler }: WarningModalType) => {
 	const qnasubmitonClick = (e: any) => {
 		setState(true);
 		e.preventDefault();
+		console.log("click");
 		solveFn({ qnasubmit: true });
 	};
 	useEffect(() => {
